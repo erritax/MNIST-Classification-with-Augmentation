8fc6d458f35941160b3751ad2dc12635d25db69b
erritax <errita.xu@gmail.com>
2025-06-25 15:39:43 -0500

    training

---
+++ train.py
@@ -0,0 +1,26 @@
+import torch
+import torch.nn.functional as F
+
+def train(model, device, train_loader, optimizer, epoch, log_interval=10):
+    
+    model.train()
+    print(f'Train Epoch: {epoch}')
+
+    # loop through batches
+    for batch_idx, (data, target) in enumerate(train_loader):
+
+        data, target = data.to(device), target.to(device)
+        optimizer.zero_grad()
+        output = model(data)
+        loss = F.nll_loss(output, target)
+        loss.backward()
+        optimizer.step()
+
+        # output progress after each batch
+        if batch_idx % log_interval == 0:
+            print(f'[{batch_idx * len(data):>5} / {len(train_loader.dataset)}'
+                    f'({100. * batch_idx / len(train_loader):>3.0f}%)]\t'
+                    f'Loss: {loss.item():.6f}')
+
+            torch.save(model.state_dict(), 'results/model.pth')
+            torch.save(optimizer.state_dict(), 'results/optimizer.pth')
